73ff7b71e31fc17adbbe24d01523d3f0a85a7972
daniella307307 <daniellaganza30@gmail.com>
2023-05-08 11:54:55 +0200

    modified the header

--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -1,32 +1,40 @@
-import { alpha,makeStyles } from "@mui/material/styles";
+import { makeStyles } from '@mui/styles';
+import { alpha } from '@mui/material';
 
-export default makeStyles((theme)=>({
-    title:{
-        display:'none',
-        [theme.breakpoints.up('sm')]:{
-            display:'block',
-        },
+export default makeStyles((theme)=>{
+  if(!theme.breakpoints || !theme.breakpoints.up){
+    console.error('Missing breakpoints or up function in theme object:', theme);
+  }
+ 
+  return {
+    title: {
+      display: 'none',
+      [theme.breakpoints('sm')]: {
+        display: 'block',
+      },
     },
-    search:{
-        position:'relative',
-        borderRadius:theme.shape.borderRadius,
-        backgroundColor:alpha(theme.palette.common.white,0.15),
-        '&:hover':{backgroundColor:alpha(theme.palette.common.white,0.25)},
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
     },
-    searchIcon:{
-        padding:theme.spacing(0,2),height:'100%',position:'absolute',PointerEvents:'none',display:'flex',alignItems:'center',justifyContent:'center', 
+    searchIcon: {
+      padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
     },
     inputRoot: {
-        color: 'inherit',
-      },
-      inputInput: {
-        padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
-      },
-      toolbar: {
-        display: 'flex', justifyContent: 'space-between',
-      },
-}))
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
+    },
+    toolbar: {
+      display: 'flex', justifyContent: 'space-between',
+    },
+  };
+})
+
