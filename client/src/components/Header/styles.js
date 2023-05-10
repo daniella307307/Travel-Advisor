48dccbd114c572f47b3734112cdc714c953a88e3
daniella307307 <daniellaganza30@gmail.com>
2023-05-10 14:40:29 +0200

    completed the header

--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -1,34 +1,38 @@
 import { makeStyles } from '@mui/styles';
-import { alpha } from '@mui/material';
-export default makeStyles((theme) => ({
+export default makeStyles(() => ({
   title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
+    display: 'Block',
+    gap:10,
   },
   search: {
     position: 'relative',
-    borderRadius: theme.borderRadius,
-    border:theme.border,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.25) },
-    marginRight: theme.spacing(2),
+    borderRadius: 5,
+    backgroundColor:'white',
+    '&:hover': { backgroundColor: 'whitesmoke' },
+    marginRight: 2,
     marginLeft: 0,
     width: '100%',
-    [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
+    
   },
   searchIcon: {
-    padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
+    padding:5,
+    color:'blue',
+    display:'flex',
+    alignItems:'center',
+    float:'right',
   },
   inputRoot: {
     color: 'inherit',
+    textAlign:'center',
+    display:'flex',
+    alignItems:'center',
   },
   inputInput: {
-    padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
+   height:1,
   },
   toolbar: {
-    display: 'flex', justifyContent: 'space-between',
+    display: 'flex',
+    justifyContent: 'space-between',
   },
 }));
 
